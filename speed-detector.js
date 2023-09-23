d5f089b23805cde5ea16458f024cfd285a9c94f7
Navashub <navasmuller01@gmail.com>
2023-09-23 10:12:39 +0300

    Updates

--- speed-detector.js
+++ speed-detector.js
@@ -1,20 +1,31 @@
 const prompt = require("prompt-sync")();
 function demeritpointsCalculator(speed)
 {
+    // speed limit declaration
     const speedLimit = 70;
+
+    // demerit points initialized to zero
     let points = 0;
 
+    // condition to check if the speed is greater than the speed limit
     if (speed <= speedLimit){
+
+        // if less it prints "OK"
         console.log("OK");
     }else {
+        // any speed above the speed limit is divided by 5 for each point
         points = Math.floor((speed - speedLimit) / 5);
         console.log(`Demerit points: ${points}`);
     }
 
+    // checks if the points are greater than 12
     if (points > 12){
+
+        // if greater than 12 it displays "License suspended"
         console.log("License suspended");
     }
 }
 
 const speed = parseInt(prompt("Enter speed of the car: "));
+// calling the function once the speed is inputed
 demeritpointsCalculator(speed);
